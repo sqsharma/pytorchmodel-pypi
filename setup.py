8c47eef37b7b491fc6eda2488028a8d20ca773b9
Shivani Sharma <sqsharma@amazon.com>
2022-06-03 15:29:19 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 setup(
     name='pytorchmodel-pypi',
     version='2.0',
-    license='Apache',
+    license='Apache License',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/sqsharma/pytorchmodel-pypi',
     keywords='pytorch model',
     install_requires=[
-          'scikit-learn',
+          'scikit-learn', 
       ],
 
 )
